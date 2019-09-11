fdb4a75f0d2d60fd0ae219fcfd756d32d2efbe03
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2019-09-11 13:58:45 +0100

    Changes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,16 +5,17 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page alredy! Wooo!');
+        return new Response('OMG! My first page already! Wooo!');
     }
 
     /**
@@ -24,6 +25,15 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show one space article: %s', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render(
+            'article/show.html.twig',
+            ['title' => ucwords(str_replace('-', ' ', $slug)), 'comments' => $comments]
+        );
     }
 }
