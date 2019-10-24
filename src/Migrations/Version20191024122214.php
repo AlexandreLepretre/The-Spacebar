1f5c718c9ede8f205fec39bca40e1bf7f1b0740c
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2019-10-24 13:24:27 +0100

    Changes

--- src/Migrations/Version20191024122214.php
+++ src/Migrations/Version20191024122214.php
@@ -4,31 +4,51 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
+ * Class Version20191024122214
+ * @package DoctrineMigrations
  */
 final class Version20191024122214 extends AbstractMigration
 {
-    public function getDescription() : string
+    /**
+     * @return string
+     */
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 
-    public function down(Schema $schema) : void
+    /**
+     * @param Schema $schema
+     * @throws DBALException
+     */
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
 
         $this->addSql('ALTER TABLE article CHANGE created_at created_at DATETIME DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
     }
