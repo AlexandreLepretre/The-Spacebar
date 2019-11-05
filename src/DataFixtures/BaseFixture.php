e3f30a82d6d0fd43894eaf8acc412e29f26c36bd
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2019-11-05 14:01:37 +0000

    Changes

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Exception;
 use Faker\Factory;
 use Faker\Generator;
 
@@ -22,6 +23,11 @@ abstract class BaseFixture extends Fixture
      * @var Generator
      */
     protected $faker;
+
+    /**
+     * @var array
+     */
+    private $referencesIndex = [];
 
     /**
      * @param ObjectManager $manager
@@ -54,5 +60,27 @@ abstract class BaseFixture extends Fixture
             // store for usage later as App\Entity\ClassName_#COUNT#
             $this->addReference($className . '_' . $i, $entity);
         }
+    }
+
+    /**
+     * @param string $className
+     * @return object
+     * @throws Exception
+     */
+    protected function getRandomReference(string $className)
+    {
+        if (!isset($this->referencesIndex[$className])) {
+            $this->referencesIndex[$className] = [];
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $className . '_') === 0) {
+                    $this->referencesIndex[$className][] = $key;
+                }
+            }
+        }
+        if (empty($this->referencesIndex[$className])) {
+            throw new Exception(sprintf('Cannot find any references for class "%s"', $className));
+        }
+        $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$className]);
+        return $this->getReference($randomReferenceKey);
     }
 }
--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,22 +4,23 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 /**
  * Class CommentFixture
  * @package App\DataFixtures
  */
-class CommentFixture extends BaseFixture
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     /**
      * @param ObjectManager $manager
      */
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Comment::class, 100, function(Comment $comment) {
+        $this->createMany(Comment::class, 100, function (Comment $comment) {
             /** @var Article $article */
-            $article = $this->getReference(Article::class . '_0');
+            $article = $this->getRandomReference(Article::class);
             $comment->setContent($this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true))
                 ->setAuthorName($this->faker->name)
                 ->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'))
@@ -27,5 +28,15 @@ class CommentFixture extends BaseFixture
         });
 
         $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [ArticleFixtures::class];
     }
 }
