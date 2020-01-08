e304bbb67cb57ffe04a973b8451ac2b835d8b920
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2020-01-08 13:48:41 +0000

    Deny Access in the Controller

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,29 +4,32 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CommentAdminController extends Controller
+/**
+ * Class CommentAdminController
+ * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
+ */
+class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @param CommentRepository $repository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
         $q = $request->query->get('q');
-
         $queryBuilder = $repository->getWithSearchQueryBuilder($q);
+        $pagination = $paginator->paginate($queryBuilder, $request->query->getInt('page', 1), 10);
 
-        $pagination = $paginator->paginate(
-            $queryBuilder, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            10/*limit per page*/
-        );
-
-        return $this->render('comment_admin/index.html.twig', [
-            'pagination' => $pagination,
-        ]);
+        return $this->render('comment_admin/index.html.twig', ['pagination' => $pagination]);
     }
 }
