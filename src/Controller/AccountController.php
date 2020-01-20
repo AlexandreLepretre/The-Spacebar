e17f1f021eb14886255af3cafda226fd23a9500e
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2020-01-20 13:32:54 +0000

    Fetch the User Object

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,16 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class AccountController
  * @package App\Controller
+ * @method User getUser()
  * @IsGranted("ROLE_USER")
  */
 class AccountController extends AbstractController
 {
     /**
      * @Route("/account", name="app_account")
+     * @param LoggerInterface $logger
      * @return Response
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->debug(sprintf('Checking account page for %s', $this->getUser()->getEmail()));
+
         return $this->render('account/index.html.twig', ['controller_name' => 'AccountController',]);
     }
 }
