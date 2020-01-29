6e12e69c76ffa8ad9aadbe562146a31163a4f6e4
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2020-01-29 13:33:52 +0000

    Serializer & API Endpoint

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,5 +28,14 @@ class AccountController extends AbstractController
         $logger->debug(sprintf('Checking account page for %s', $this->getUser()->getEmail()));
 
         return $this->render('account/index.html.twig', ['controller_name' => 'AccountController',]);
+    }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     * @return JsonResponse
+     */
+    public function accountApi()
+    {
+        return $this->json($this->getUser(), 200, [], ['groups' => ['main']]);
     }
 }
