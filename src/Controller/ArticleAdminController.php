6e70beb2828ee93298a35a85171d88d91a057675
Alex Lepretre <alex.lepretre@sportpursuit.co.uk>
2020-02-06 13:29:49 +0000

    Voters

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,20 +16,10 @@ class ArticleAdminController extends AbstractController
     /**
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
-     * @param EntityManagerInterface $em
-     * @return Response
      */
-    public function new(EntityManagerInterface $em)
+    public function new()
     {
         die('todo');
-
-        return new Response(
-            sprintf(
-                'Hiya! New Article id: #%d slug: %s',
-                $article->getId(),
-                $article->getSlug()
-            )
-        );
     }
 
     /**
@@ -40,7 +28,7 @@ class ArticleAdminController extends AbstractController
      */
     public function edit(Article $article)
     {
-        if ($article->getAuthor() != $this->getUser() && !$this->isGranted('ROLE_ADMIN_ARTICLE')) {
+        if (!$this->isGranted('MANAGE', $article)) {
             throw $this->createAccessDeniedException('No access!');
         }
 
